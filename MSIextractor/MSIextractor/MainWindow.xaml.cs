339ea2be696b314d58173951eef75b0a85b01e6c
Chandra Sekhar Ravuri <chandrasekhar.r18@gmail.com>
2020-05-05 20:23:56 +0530

    Core functionality added, ready for beta testing

--- MSIextractor/MSIextractor/MainWindow.xaml.cs
+++ MSIextractor/MSIextractor/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace MSIextractor
 {
@@ -22,6 +13,48 @@ namespace MSIextractor
         public MainWindow()
         {
             InitializeComponent();
+        }        
+
+        private void msiSelect_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "MSI files (*.msi)|*.msi|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if(openFileDialog.ShowDialog() == true)
+            {
+                txtMSIPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void ResetControls()
+        {
+            txtMSIPath.IsEnabled = false;
+            txtSaveFolder.IsEnabled = false;
+        }
+
+        private void saveFolderSelect_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowNewFolderButton = true;
+
+
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
+            {
+                txtSaveFolder.Text = folderBrowserDialog.SelectedPath;
+            }
+
+
+        }
+
+        private void convert_Click(object sender, RoutedEventArgs e)
+        {
+            ResetControls();
+            string exeString = "msiexec";
+            string argString = $"/a \"{txtMSIPath.Text}\" /qb TARGETDIR=\"{txtSaveFolder.Text}\"";
+            Process.Start(exeString, argString);
         }
     }
+
+    
 }
